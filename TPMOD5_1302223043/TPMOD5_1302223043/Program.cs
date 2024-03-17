290b15f82a2d3b09df2fbc7b40c2af354a819943
cruzte4 <162765665+cruzte4@users.noreply.github.com>
2024-03-17 22:17:33 +0700

    Add files via upload

---
+++ TPMOD5_1302223043/TPMOD5_1302223043/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class DataGeneric<T>
+{
+    private T data;
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: " + data);
+    }
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataGeneric<string> dataString = new DataGeneric<string>("132223043");
+        dataString.PrintData();
+
+        DataGeneric<int> dataInt = new DataGeneric<int>(132223043);
+        dataInt.PrintData();
+    }
+}
